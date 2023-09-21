eefc465573f66bd08b1a963b1edb69f942f3dde0
Yurii Kuchmenko <kuazakotl1@gmail.com>
2023-09-22 00:10:17 +0300

    Register models in the admin

--- train_station/admin.py
+++ train_station/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    CrewMember,
+    Station,
+    Route,
+    TrainType,
+    Train,
+    Journey,
+    Order,
+    Ticket,
+)
+
+
+admin.site.register(CrewMember)
+admin.site.register(Station)
+admin.site.register(Route)
+admin.site.register(TrainType)
+admin.site.register(Train)
+admin.site.register(Journey)
+admin.site.register(Order)
+admin.site.register(Ticket)
