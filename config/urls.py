338fd79507e3a07a1afd0fd0dd9ba9af8a591a2b
Yurii Kuchmenko <kuazakotl1@gmail.com>
2023-09-25 16:32:03 +0300

    Fix media files; change URL

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path(
-        "api/train_station/",
+        "api/train-station/",
         include("train_station.urls", namespace="train_station")
     ),
     path("api/user/", include("user.urls", namespace="user")),
